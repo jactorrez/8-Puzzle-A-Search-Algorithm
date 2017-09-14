58c7e31d3c2caa34e771a37b8534528f8e46aa12
Javier Torrez <javierctorrez@gmail.com>
2017-09-13 21:44:04 -0400

    cleaned up and optimized Board code

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Board {
 	
 	public int[][] board;
+	private int[] blankPosition = new int[2];
 	private final int inversions;
 	private final int N;
 	private final int boardSize;
@@ -19,15 +20,16 @@ public class Board {
 			}
 		}
 		
+		getBlankPosition();
 		this.inversions = inversions();
 	}
 	
-	// Returns board size
+	// Returns board size: O(1)
 	public int size(){
 		return boardSize;
 	}
 	
-	// Returns number of blocks out of place
+	// Returns number of blocks out of place: O(N^2)
 	public int hamming(){
 		int amount = 0; 
 		
@@ -42,13 +44,11 @@ public class Board {
 					amount++;
 				}
 			}
-			
 		}
-		
 		return amount;
 	}
 	
-	// Returns sum of Manhattan distances between blocks and goal
+	// Returns sum of Manhattan distances between blocks and goal: O(N^2)
 	public int manhattan(){
 		int sum = 0;
 		
@@ -64,7 +64,6 @@ public class Board {
 				int goalCol = (val-1) % N;
 	
 				int manhatDist = Math.abs(row - goalRow) + Math.abs(col - goalCol);
-				System.out.println("Manhattan distance for " + board[i][j] + " is " + manhatDist);
 				sum += manhatDist;
 			}
 		}
@@ -77,25 +76,18 @@ public class Board {
 		return (inversions() == 0);
 	}
 	
-	// Checks if this board is solvable 
+	// Checks if this board is solvable: O(1)
 	public boolean isSolvable(){
-		System.out.println("board size is: " + boardSize + " and number of inversions " + inversions);
 		// Board size is odd
 		if(boardSize / 2 != 0){
-			System.out.println("board size is odd");
 			// Parity of inversions is odd, will remain odd, so it is unsolvable
 			if(inversions % 2 != 0){
-				System.out.println("inversions are odd");
-
 				return false;
 			} else {
-				System.out.println("inversions are even");
 				return true;
 			}
 		} else {
-			System.out.println("board size is even");
-
-			int sum = inversions + blankDimensions()[0];
+			int sum = inversions + blankPosition[0];
 			
 			if(sum % 2 == 0){
 				return false;
@@ -105,7 +97,7 @@ public class Board {
 		}
 	}
 	
-	// Checks if this board equals board y
+	// Checks if this board equals board y: O(N^2)
 	public boolean equals(Board y){
 		int[][] other = y.board;
 		int otherBoardSize = y.boardSize;
@@ -126,40 +118,39 @@ public class Board {
 		return isEqual;
 	}
 	
-	// Returns all neighboring boards
+	// Returns all neighboring boards: O(1)
 	public Iterable<Board> neighbors(){
 		ArrayList<Board> neighbors = new ArrayList<>();
-		int[] blankPos = blankDimensions();
-		int row = blankPos[0];
-		int col = blankPos[1];
+		int row = blankPosition[0];
+		int col = blankPosition[1];
 		boolean hasRightBlock = (col + 1 <= N-1);
 		boolean hasLeftBlock = (col - 1 >= 0);
 		boolean hasTopBlock = (row - 1 >= 0);
 		boolean hasBottomBlock = (row + 1 <= N-1);
 		
 		if(hasRightBlock){
-			Board newBoard = new Board(makeMove(blankPos, row, col + 1));
+			Board newBoard = new Board(makeMove(row, col + 1));
 			neighbors.add(newBoard);
 		} 
 		
 		if(hasLeftBlock){
-			Board newBoard = new Board(makeMove(blankPos, row, col - 1));
+			Board newBoard = new Board(makeMove(row, col - 1));
 			neighbors.add(newBoard);
 		}
 		
 		if(hasTopBlock){
-			Board newBoard = new Board(makeMove(blankPos, row - 1, col));
+			Board newBoard = new Board(makeMove(row - 1, col));
 			neighbors.add(newBoard);
 		}
 		
 		if(hasBottomBlock){
-			Board newBoard = new Board(makeMove(blankPos, row + 1, col));
+			Board newBoard = new Board(makeMove(row + 1, col));
 			neighbors.add(newBoard);
 		}
 		return neighbors;
 	}
 	
-	// Outputs string representation of this board
+	// Outputs string representation of this board: O(N^2)
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
 		for(int r = 0; r < N; r++){
@@ -176,6 +167,7 @@ public class Board {
 	
 	/**
 	 * Calculates number of inversions present in current board
+	 * 
 	 * @return number of inversions
 	 */
 	private int inversions(){
@@ -200,38 +192,38 @@ public class Board {
 	
 	/**
 	 * Finds the position of the "blank" block in the board
-	 * @return
+	 * 
+	 * @return integer array containing the row at index 0 and column at index 1
 	 */
-	private int[] blankDimensions(){
-		int[] index = new int[2];
+	private void getBlankPosition(){
 		boolean found = false;
 		
 		for(int r = 0; r < N && !found; r++){
 			for(int c = 0; c < N; c++){
 				if(board[r][c] == 0){
-					index[0] = r;
-					index[1] = c;
+					blankPosition[0] = r;
+					blankPosition[1] = c;
 					found = true;
 					break;
 				}
 			}
 		}
-		
-		return index;
+
 	}
 	
 	/**
 	 * Creates an instance of a neighbor for this board
+	 * 
 	 * @param blank		position of blank block
 	 * @param row		row location of item to move to blank block position
 	 * @param col		column location of item to move to blank block position
 	 * @return instance of a Board representing a neighbor of current board
 	 */
-	private int[][] makeMove(int[] blank, int row, int col){
+	private int[][] makeMove(int row, int col){
 		int[][] boardCopy = copyOfBoard();
 		
-		int blankRow = blank[0];
-		int blankCol = blank[1];
+		int blankRow = blankPosition[0];
+		int blankCol = blankPosition[1];
 		
 		boardCopy[blankRow][blankCol] = boardCopy[row][col];
 		boardCopy[row][col] = 0;
@@ -241,6 +233,7 @@ public class Board {
 	
 	/**
 	 * Makes copy of the current board
+	 * 
 	 * @return copy of the current board
 	 */
 	
@@ -258,28 +251,24 @@ public class Board {
 	
 	// Unit tests 
 	public static void main(String[] args){
-		int[][] test2d = {{1,3,4},
+		int[][] test = {{1,3,4},
 						  {8,0,7},
 						  {5,6,2}};
-		
-		int[][] copy = {{1,3,4},
-				  {8,0,7},
-				  {5,6,2}};
-		
-		int[][] testgoal = {{1,2,3},
+
+		int[][] goal = {{1,2,3},
 				  		    {4,5,6},
 				  		    {7,8,0}};
 
 		
-		Board testBoard = new Board(test2d);
-		Board testGoal = new Board(testgoal);
-		Board testCopy = new Board(copy);
+		Board testBoard = new Board(test);
+		Board testGoal = new Board(goal);
 		
 		System.out.println("Current board...");
 		System.out.println(testBoard);
 		
 		System.out.println("Neighbors...");
 		Iterable<Board> neighbors = testBoard.neighbors();
+		
 		for(Board b : neighbors){
 			System.out.println(b);
 		}
